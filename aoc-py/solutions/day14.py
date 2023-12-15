f0800de312d226f2d083d2fe6f56dc8eded6a7cd
Tom-the-Bomb <thello522@gmail.com>
2023-12-14 19:47:31 -0500

    day 14 fix

--- aoc-py/solutions/day14.py
+++ aoc-py/solutions/day14.py
@@ -17,49 +17,69 @@ class Day14(Solution):
     NAME: ClassVar[str] = 'Parabolic Reflector Dish'
 
     def _transpose(self, grid: Grid) -> Grid:
+        """Transposes the grid"""
         return [list(row) for row in zip(*grid)]
     
     def _reverse_rows(self, grid: Grid) -> Grid:
-        return [row[::-1] for row in grid]
+        """Reverses each row in the grid"""
+        for row in grid:
+            row.reverse()
+        return grid
 
     def _tilt_lever(self, grid: Grid) -> Grid:
-        for col in grid:
-            for i, element in enumerate(col):
+        """Pushes all rounded rocks to the left side (west) of the matrix"""
+        for row in grid:
+            for i, element in enumerate(row):
+                # goes from left to right for each row and finds all rounded rocks
                 if element == 'O':
+                    # for each rounded rock, traverse backwards in the row
+                    # and if the tile before it is empty we can swap and move the rock to the left
+                    # we keep doing so until we hit a non empty tile
                     for t in range(i, 0, -1):
-                        if col[t - 1] == '.':
-                            col[t - 1], col[t] = col[t], col[t - 1]
+                        if row[t - 1] == '.':
+                            # is empty, swap
+                            row[t - 1], row[t] = row[t], row[t - 1]
                         else:
+                            # non empty tile, we are done pushing it
                             break
         return grid
     
     def _tilt_north(self, grid: Grid) -> Grid:
+        """Tilts the rocks north (up) (transpose first)"""
         grid = self._transpose(grid)
         grid = self._tilt_lever(grid)
         return self._transpose(grid)
     
     def _tilt_south(self, grid: Grid) -> Grid:
+        """Tilts the rocks south (down)
+        (same as north; but reverse the columns
+        (which are rows after transposal))
+        """
         grid = self._transpose(grid)
         grid = self._reverse_rows(grid)
         grid = self._tilt_lever(grid)
         grid = self._reverse_rows(grid)
         return self._transpose(grid)
     
-    def _tilt_west(self, grid: Grid) -> Grid:
-        return self._tilt_lever(grid)
-    
     def _tilt_east(self, grid: Grid) -> Grid:
+        """Tilts the rocks east (right)
+        Same as the default (left) but reverses the rows
+        """
         grid = self._reverse_rows(grid)
         grid = self._tilt_lever(grid)
         return self._reverse_rows(grid)
     
     def _cycle(self, grid: Grid) -> Grid:
         grid = self._tilt_north(grid)
-        grid = self._tilt_west(grid)
+        # west = left (no need for pre manipulation)
+        grid = self._tilt_lever(grid)
         grid = self._tilt_south(grid)
         return self._tilt_east(grid)
     
     def _get_load(self, grid: Grid) -> int:
+        """Calculates the load
+        by counting the rounded boulders per row
+        """
         n_rows = len(grid)
         return sum(
             row.count('O') * (n_rows - i)
@@ -74,17 +94,33 @@ class Day14(Solution):
     def part_two(self, inp: str) -> int:
         grid = [list(row) for row in inp.splitlines()]
         
+        # `cycles` will contain the extra non-repeating terms
+        # + 1 period
         cycles = [grid]
+        # contains the index of the element that first begins the repeating sequence
+        # also equal to the length of the extra non-repeating prefixing chunk
         start = 0
         while True:
+            # generates next term
             next_term = self._cycle(cycles[-1])
+            # this marks the end of our first cycle
+            # since we have already seen `next_term` before when it was the start of the cycle
             if next_term in cycles:
+                # set `start` to the index of the start of the first cycle
                 start = cycles.index(next_term)
                 break
             else:
                 cycles.append(next_term)
-        period = len(cycles) - start
-        for _ in range((1_000_000_000 - start) % period + start):
+
+        for _ in range(
+            # the total length of the repeating chunk
+            (1_000_000_000 - start)
+            # modulo by the period of the sequence
+            # (gets the remainder at the end that does not complete another full cycle)
+            % (len(cycles) - start)
+            # add back the non-repeating chunk's length
+            + start
+        ):
             grid = self._cycle(grid)
         return self._get_load(grid)
 
@@ -92,5 +128,5 @@ class Day14(Solution):
         print('Part 1:', p1 := self.part_one(inp))
         print('Part 2:', p2 := self.part_two(inp))
 
-        #assert p1 == 35210
-        #assert p2 == 31974
+        assert p1 == 111339
+        assert p2 == 93736
